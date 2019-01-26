4b3a3b4876a69b9192366b3a936b0172e551695a
Nimish <nimishsantosh107@icloud.com>
2019-01-26 19:09:02 +0530

    face_eye_recog under dev

---
+++ python/face_eye_recog.py
@@ -0,0 +1,55 @@
+# Face Recognition
+
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') # We load the cascade for the face.
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml') # We load the cascade for the eyes.
+
+def detect(gray, frame): # We create a function that takes as input the image in black and white (gray) and the original image (frame), and that will return the same image with the detector rectangles. 
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5) # We apply the detectMultiScale method from the face cascade to locate one or several faces in the image.
+    for (x, y, w, h) in faces: # For each detected face:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2) # We paint a rectangle around the face.
+        roi_gray = gray[y:y+h, x:x+w] # We get the region of interest in the black and white image.
+        roi_color = frame[y:y+h, x:x+w] # We get the region of interest in the colored image.
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3) # We apply the detectMultiScale method to locate one or several eyes in the image.
+        if(len(eyes)>0):    
+            for (ex, ey, ew, eh) in eyes: # For each detected eye:
+                cv2.rectangle(roi_color,(ex, ey),(ex+ew, ey+eh), (0, 255, 0), 2) # We paint a rectangle around the eyes, but inside the referential of the face.
+                roi = roi_color[ex: ey, ex+ew: ey+eh]
+                rows, cols, _ = roi.shape
+                gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+                gray_roi = cv2.GaussianBlur(gray_roi, (7, 7), 0)
+
+                _, threshold = cv2.threshold(gray_roi, 3, 255, cv2.THRESH_BINARY_INV)
+                _, contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
+
+                for cnt in contours:
+                    (x, y, w, h) = cv2.boundingRect(cnt)
+
+                    #cv2.drawContours(roi, [cnt], -1, (0, 0, 255), 3)
+                    cv2.rectangle(roi, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                    cv2.line(roi, (x + int(w/2), 0), (x + int(w/2), rows), (0, 255, 0), 2)
+                    cv2.line(roi, (0, y + int(h/2)), (cols, y + int(h/2)), (0, 255, 0), 2)
+                    break
+
+                #cv2.imshow("Threshold", threshold)
+                cv2.imshow("gray roi", gray_roi)
+                #cv2.imshow('Roi', roi)
+                key = cv2.waitKey(30)
+                if key == 27:
+                   break
+    return frame # We return the image with the detector rectangles.
+
+video_capture = cv2.VideoCapture(0) # We turn the webcam on.
+
+while True: # We repeat infinitely (until break):
+    _, frame = video_capture.read() # We get the last frame.
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # We do some colour transformations.
+    canvas = detect(gray, frame) # We get the output of our detect function.
+    cv2.imshow('Video', canvas) # We display the outputs.
+    if cv2.waitKey(1) & 0xFF == ord('q'): # If we type on the keyboard:
+        break # We stop the loop.
+
+video_capture.release() # We turn the webcam off.
+cv2.destroyAllWindows() # We destroy all the windows inside which the images were displayed.
